e13faa60e0fa9cf91dfbda44ee88fa3f91a23928
kushal-10 <kushalkoshti11@gmail.com>
2023-11-08 14:55:53 +0100

    update app

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 # Application file for Gradio App for OpenAI Model
 
-#options - 'output_reports/reports_n/faiss_index' - n=1..9 OR 'combined/faiss_index'
-dir = 'paper_combined/faiss_index'
-
-demo_query = "What important topics are discussed in this report?"
-
 import gradio as gr
 import time
 import pandas as pd
 from lc_base.chain import openai_chain
+import os
+
+dir = os.path.join("outputs", "combined", "papers_gpt4turbo_mapred5", "faiss_index")
 
 title = """<h1 align="center">Chat</h1>"""
 description = """<br><br><h3 align="center">This is a literature chat model, which can currently answer questions to New Data provided.</h3>"""
@@ -19,18 +17,18 @@ def user(user_message, history):
 def respond(message, chat_history):
     question = str(message)
     chain = openai_chain(inp_dir=dir)
-    output = chain.get_response(query=question, k=10, type="map_reduce")
+    output = chain.get_response(query=question, k=1, model_name="gpt-4-1106-preview", type="stuff")
 
-    splits = dir.split("/")
-    pdf_dir = "data_" + splits[0] + "/data_" + splits[1]
-    name = pdf_dir
-    store_data = {
-        "query" : question,
-        "paper" : name,
-        "response" : output
-    }
-    data_df = pd.DataFrame(store_data, index=[0])
-    data_df.to_csv("paper_csvs/" + splits[1] + ".csv")
+    # splits = dir.split("/")
+    # pdf_dir = "data_" + splits[0] + "/data_" + splits[1]
+    # name = pdf_dir
+    # store_data = {
+    #     "query" : question,
+    #     "paper" : name,
+    #     "response" : output
+    # }
+    # data_df = pd.DataFrame(store_data, index=[0])
+    # data_df.to_csv("paper_csvs/" + splits[1] + ".csv")
 
     bot_message = output
     chat_history.append((message, bot_message))
@@ -47,8 +45,8 @@ with gr.Blocks(theme=gr.themes.Soft(primary_hue="emerald", neutral_hue="slate"))
 
     gr.Examples([
         ["What are the challenges and opportunities of AI in supply chain management?"],
-        ["What does this report talk about?"],
-        ["What does this paper talk about? Please explain in detail."],
+        ["What does these reports talk about?"],
+        ["What does these papers talk about? Please explain in detail."],
         ["What is the impact of using AI in supply chain management?"]
 
     ], inputs=msg, label= "Click on any example to copy in the chatbox"
