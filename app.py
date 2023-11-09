bc6ea56487afae3195a324a0b2eef122f97d66b5
kushal-10 <kushalkoshti11@gmail.com>
2023-11-09 19:13:29 +0100

    add api_key input

--- app.py
+++ app.py
@@ -5,11 +5,16 @@ import time
 import pandas as pd
 from lc_base.chain import openai_chain
 import os
+import requests
 
 dir = os.path.join("outputs", "combined", "papers_gpt4turbo_mapred5", "faiss_index")
 
 title = """<h1 align="center">Chat</h1>"""
 description = """<br><br><h3 align="center">This is a literature chat model, which can currently answer questions to New Data provided.</h3>"""
+
+def save_api_key(api_key):
+    os.environ['OPENAI_API_KEY'] = str(api_key)
+    return f"API Key saved in the environment: {api_key}"
 
 def user(user_message, history):
     return "", history + [[user_message, None]]
@@ -17,18 +22,7 @@ def user(user_message, history):
 def respond(message, chat_history):
     question = str(message)
     chain = openai_chain(inp_dir=dir)
-    output = chain.get_response(query=question, k=1, model_name="gpt-4-1106-preview", type="stuff")
-
-    # splits = dir.split("/")
-    # pdf_dir = "data_" + splits[0] + "/data_" + splits[1]
-    # name = pdf_dir
-    # store_data = {
-    #     "query" : question,
-    #     "paper" : name,
-    #     "response" : output
-    # }
-    # data_df = pd.DataFrame(store_data, index=[0])
-    # data_df.to_csv("paper_csvs/" + splits[1] + ".csv")
+    output = chain.get_response(query=question, k=1, model_name="gpt-3.5-turbo", type="stuff")
 
     bot_message = output
     chat_history.append((message, bot_message))
@@ -37,6 +31,10 @@ def respond(message, chat_history):
 
 with gr.Blocks(theme=gr.themes.Soft(primary_hue="emerald", neutral_hue="slate")) as chat:
     gr.HTML(title)
+                 
+    api_key_input = gr.Textbox(lines=1, label="Enter your OpenAI API Key")
+    api_key_input_submit = api_key_input.submit(save_api_key, [api_key_input])
+
     chatbot = gr.Chatbot().style(height=750)
     msg = gr.Textbox(label="Send a message", placeholder="Send a message",
                              show_label=False).style(container=False)
